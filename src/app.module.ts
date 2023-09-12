b9cd4cb4630dd4b7b458237121f8b17861f6fecb
gustavojms <gustavo.joset0@hotmail.com>
2023-09-12 16:59:53 -0300

    feat: adicionando guard para rotas publicas

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,10 +5,19 @@ import { UsersModule } from './modules/users/users.module';
 import { ConfigModule } from '@nestjs/config';
 import { AuthModule } from './modules/auth/auth.module';
 import { PrismaService } from './database/prisma.service';
+import { APP_GUARD } from '@nestjs/core';
+import { JwtAuthGuard } from './modules/auth/jwt-auth.guard';
 
 @Module({
   imports: [UsersModule, AuthModule, ConfigModule.forRoot({ isGlobal: true })],
   controllers: [AppController],
-  providers: [AppService, PrismaService],
+  providers: [
+    AppService,
+    PrismaService,
+    {
+      provide: APP_GUARD,
+      useClass: JwtAuthGuard,
+    },
+  ],
 })
 export class AppModule {}
--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -1,17 +1,20 @@
 import { Body, Controller, HttpCode, HttpStatus, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { CreateUserDto } from '../users/dto/create-user.dto';
+import { Public } from './public-auth';
 
 @Controller('api/auth')
 export class AuthController {
   constructor(private authService: AuthService) {}
 
+  @Public()
   @HttpCode(HttpStatus.CREATED)
   @Post('/register')
   register(@Body() data: CreateUserDto) {
     return this.authService.register(data);
   }
 
+  @Public()
   @HttpCode(HttpStatus.OK)
   @Post('/login')
   login(@Body() data: CreateUserDto) {
