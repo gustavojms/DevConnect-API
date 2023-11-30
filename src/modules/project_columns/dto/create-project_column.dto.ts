d41cd4275098c90d43f21224dca679f1ed57affa
gustavojms <gustavo.joset0@hotmail.com>
2023-11-29 23:12:03 -0300

    removing unnecessary model

--- src/modules/project_columns/dto/create-project_column.dto.ts
+++
@@ -1,6 +0,0 @@
-export class CreateProjectColumnDto {
-  projectId: number;
-  columnId: number;
-  createdAt: Date;
-  updatedAt: Date;
-}
--- src/modules/project_columns/entities/project_column.entity.ts
+++
@@ -1,9 +0,0 @@
-import { ProjectColumn } from '@prisma/client';
-
-export class ProjectColumnEntity implements ProjectColumn {
-  projectColumnId: number;
-  projectId: number;
-  columnId: number;
-  createdAt: Date;
-  updatedAt: Date;
-}
